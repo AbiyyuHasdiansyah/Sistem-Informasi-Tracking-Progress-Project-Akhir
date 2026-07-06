278be45517abebfbbe17143eef17bcd39cbe971c
AbiyyuHasdiansyah <c050424008@mahasiswa.poliban.ac.id>
2026-07-06 15:30:54 +0800

    Clear login form defaults

--- tracking_progress_project_akhir_mobile/lib/screens/login_screen.dart
+++ tracking_progress_project_akhir_mobile/lib/screens/login_screen.dart
@@ -12,8 +12,8 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController(text: 'dian.pratama@poliban.ac.id');
-  final _passwordController = TextEditingController(text: 'password123');
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   bool _loading = false;
   String? _error;
 
